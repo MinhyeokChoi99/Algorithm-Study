fef8aa14f4dadf61c207fce3a4500a1943ccbd83
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-07 23:07:40 +0900

    Create 10609_TodayDate.java

---
+++ baekjoonbronze/10609_TodayDate.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze;
+
+import java.time.LocalDate;
+
+public class BOJ10699 {
+
+    public static void main(String[] args) {
+        LocalDate lt =  LocalDate.now();
+        System.out.println(lt);
+    }
+}
