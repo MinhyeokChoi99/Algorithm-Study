78f7188a456d8cee9dd939a5ceef2d3e2b1991f8
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-07 23:09:34 +0900

    Create 7287_Register.java

---
+++ baekjoon/bronze/7287_Register.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze;
+
+
+
+public class BOJ7287 {
+
+    public static void main(String[] args) {
+        System.out.println("2");
+        System.out.println("bbbim5656");
+    }
+}
