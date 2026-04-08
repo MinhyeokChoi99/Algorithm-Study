491df72767c96f4195f893a0d2fcbc373cd97e3f
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-09 03:00:27 +0900

    Update StackMain.java

--- DoIt/Stack/StackMain.java
+++ DoIt/Stack/StackMain.java
@@ -1,18 +1,18 @@
 package stack;
 
 public class StackMain {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		
-		Mystack ms = new Mystack(3);
-		ms.push(1); //size = 1
-		ms.push(2);//size = 2
-		ms.push(3);
-		ms.pop();
-		
-		
-		
-	
-	}
-
+        Mystack ms = new Mystack(3);
+        ms.push(1);
+        ms.push(2);
+        ms.push(3);
+        System.out.println(ms.peek());
+        ms.pop();
+        ms.pop();
+        ms.pop();
+        System.out.println(ms.size());
+        System.out.println(ms.isEmpty());
+    }
 }
+
