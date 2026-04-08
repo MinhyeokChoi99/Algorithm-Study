fdeff84c45d3236e5108eff761cee6ed540fa8d8
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-09 01:11:03 +0900

    Create 9498_Score.java

---
+++ baekjoon/bronze/9498_Score.java
@@ -0,0 +1,25 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9498 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int score = Integer.parseInt(s);
+
+        if(score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
