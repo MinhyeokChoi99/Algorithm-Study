d0f469e5023abef9729f83dc051f23b2f038a99c
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-11 00:15:09 +0900

    Create 2884_AlarmClock.java

---
+++ baekjoon/bronze/2884_AlarmClock.java
@@ -0,0 +1,30 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(m >= 45) {
+            m = m -45;
+            System.out.println(h + " " + m);
+        } else {
+            if(h == 0) {
+                h = 23;
+                m = 60 - Math.abs(m-45);
+                System.out.println((h + " " + m));
+            } else{
+                h -= 1;
+                m = 60 - Math.abs(m-45);
+                System.out.println((h + " " + m));
+            }
+        }
+    }
+}
