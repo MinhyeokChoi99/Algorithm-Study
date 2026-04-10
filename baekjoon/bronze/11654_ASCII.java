bc217d2e91f59a713386666432e0aaf9ddc076be
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-10 22:22:14 +0900

    Create 11654_ASCII.java

---
+++ baekjoon/bronze/11654_ASCII.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int a = s.charAt(0);
+        System.out.println(a);
+
+    }
+}
