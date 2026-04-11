b3cf312a23616ae7ccd4a04450ad729fff3abef1
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-12 03:20:17 +0900

    Create 9086_String.java

---
+++ baekjoon/bronze/9086_String.java
@@ -0,0 +1,19 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9086 {
+    static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < number; i++) {
+            String str = br.readLine();
+            String first = String.valueOf(str.charAt(0));
+            String second = String.valueOf(str.charAt(str.length() - 1));
+            System.out.println(first + second);
+        }
+    }
+}
