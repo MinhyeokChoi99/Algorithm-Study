0531e191392e984f5b7ea8c0600ca359d6577d7a
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-12 03:24:34 +0900

    Update 9086_String.java

--- baekjoon/bronze/9086_String.java
+++ baekjoon/bronze/9086_String.java
@@ -9,11 +9,15 @@ public class BOJ9086 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(br.readLine());
         
+        // for (int i = 0; i < number; i++) {
+        //     String str = br.readLine();
+        //     String first = String.valueOf(str.charAt(0));
+        //     String second = String.valueOf(str.charAt(str.length() - 1));
+        //     System.out.println(first + second);
+        // }
         for (int i = 0; i < number; i++) {
             String str = br.readLine();
-            String first = String.valueOf(str.charAt(0));
-            String second = String.valueOf(str.charAt(str.length() - 1));
-            System.out.println(first + second);
+            System.out.println(str.charAt(0) + "" + str.charAt(str.length()-1));
         }
     }
 }
