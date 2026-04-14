044c497bc1d14a2b7c138df22b4880cffced97cb
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-15 00:15:43 +0900

    Update 2231_SplitSum.java

--- baekjoon/bronze/2231_SplitSum.java
+++ baekjoon/bronze/2231_SplitSum.java
@@ -5,22 +5,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ2231 {
-    static void main(String[] args) throws IOException {
-//        int n = 123; // 예시
-//        int splitSum = n + (n / 100) + (n % 100 / 10)  + (n % 100 % 10); // splitSum 이 주어졌을 때 어떻게 n을 구할것이냐?
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.parseInt(br.readLine());
         int result = 0;
+
+
+
+        // 1부터 input 사이에 조건을 만족하는 결과가 있는지 찾기
         for (int j = 1; j <= input; j++) {
-            if(j + (j / 100) + (j % 100 / 10)  + (j % 100 % 10) == input) {
+            if(j + getSum(j) == input) {
                 result = j;
-                break;
+                break; // 가장 작은 조건을 만족했을 때 반복 종료
             }
         }
         System.out.println(result);
 
 
 
+    }
+
+    // 각 자리 숫자의 합을 구함
+    private static int getSum(int input) {
+        int sum = 0;
+        int temp = input;
+        while(temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        return sum;
     }
 }
