7ac07e321ae9239fb0fa9e9652530fac13da47c2
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-04-28 15:30:11 +0900

    Create 43165_타겟넘버.java

---
+++ Programmers/lv2/43165_타겟넘버.java
@@ -0,0 +1,18 @@
+class Solution {
+	static int answer = 0;
+	
+    public int solution(int[] numbers, int target) {
+    	dfs(0,0,target,numbers); // dfs(합,깊이,목표숫자,배열)
+    	return answer;
+    	} 
+    public static void dfs(int sum, int depth, int target, int[]numbers) {
+    	if(depth == numbers.length) {
+    		if(sum == target) {
+    			answer++;
+    		}
+    		return;
+    	}
+    	dfs(sum + numbers[depth], depth + 1, target, numbers);
+    	dfs(sum - numbers[depth], depth + 1, target, numbers);
+    }
+}
