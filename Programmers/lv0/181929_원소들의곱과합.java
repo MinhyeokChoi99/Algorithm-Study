d244c9058a3224b794a51cc31d48b9a1662da7b7
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-05-04 19:29:53 +0900

    Create 181929_원소들의곱과합.java

---
+++ Programmers/lv0/181929_원소들의곱과합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int multi = 1;
+        for(int num : num_list) {
+            sum += num;
+            multi *= num;   
+        }
+        
+        return (sum*sum) > multi ? 1 : 0;
+      
+    }
+}
