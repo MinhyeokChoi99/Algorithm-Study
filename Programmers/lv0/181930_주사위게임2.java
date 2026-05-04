be00119eefda42359f717b56f5f8cd827bdb33d8
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-05-04 13:58:15 +0900

    Create 181930_주사위게임2.java

---
+++ Programmers/lv0/181930_주사위게임2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if(a == b && b ==c) {
+            return (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c) * (a+b+c);   
+        } else if(a != b && b != c && a!= c) {
+            return a + b + c;
+        } else {
+            return (a+b+c) * (a*a+b*b+c*c);
+        }
+    
+    }
+}
