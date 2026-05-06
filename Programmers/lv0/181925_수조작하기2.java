df833092400c566668e66b8a0e7a6e9ea9654b82
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-05-07 01:55:29 +0900

    Create 181925_수조작하기2.java

---
+++ Programmers/lv0/181925_수조작하기2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(int[] numLog) {
+        
+        String answer = "";
+        int start = numLog[0]; 
+        
+        for(int i = 0; i < numLog.length - 1; i ++) {
+            int operation = numLog[i+1] - numLog[i];
+            switch(operation) {
+                case 1:
+                    answer += "w";
+                    break;
+                case -1:
+                    answer += "s";
+                    break;
+                case 10:
+                    answer += "d";
+                    break;
+                case -10:
+                    answer += "a";
+                    break;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
