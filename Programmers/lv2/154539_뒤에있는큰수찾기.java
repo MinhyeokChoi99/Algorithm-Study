3b0aa0608c1557b1681d4cc9b5223638a6cb3f2f
Minhyeok Choi <84328819+MinhyeokChoi99@users.noreply.github.com>
2026-05-07 15:42:32 +0900

    Update 154539_뒤에있는큰수찾기.java

--- Programmers/lv2/154539_뒤에있는큰수찾기.java
+++ Programmers/lv2/154539_뒤에있는큰수찾기.java
@@ -6,22 +6,22 @@ class Solution {
         // 결과를 담을 배열 생성
         int[] result = new int[numbers.length];
         //작업을 시행할 스택 생성
-        Deque<int[]> q = new ArrayDeque<>();
+        Deque<int[]> stack = new ArrayDeque<>();
         
         //앞에서부터 하나씩 확인하며 한 번 순회를 마쳤을 때 결과를 반환
         for(int i = 0; i < numbers.length; i ++) {
             // 스택이 비었는지 확인하고 스택 맨 위의 값과 크기 비교
-            while(!q.isEmpty() && q.peek()[0] < numbers[i]) {
-                int[] cur = q.poll();
+            while(!stack.isEmpty() && stack.peek()[0] < numbers[i]) {
+                int[] cur = stack.poll();
                 result[cur[1]] = numbers[i];
             }
             // 자기자신을 스택에 추가
-            q.push(new int[]{numbers[i],i});
+            stack.push(new int[]{numbers[i],i});
         }
         
         // 끝까지 남은 값들 스택에서 초기화
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
+        while(!stack.isEmpty()) {
+            int[] cur = stack.poll();
             result[cur[1]] = -1;
         }
         
